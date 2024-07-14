8f1551df02c2162bc99f636a20e8bd0f8fb77721
ajaypawar2211 <ajaypawar6225@gmail.com>
2024-07-14 14:41:36 +0530

    customer-added

--- eshopdjango-master/store/models/customer.py
+++ eshopdjango-master/store/models/customer.py
@@ -4,7 +4,7 @@ from django.core.validators import MinLengthValidator
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=15)
+    phone = models.TextField()
     email = models.EmailField()
     password = models.CharField(max_length=500)
 
